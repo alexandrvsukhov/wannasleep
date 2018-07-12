3f3ad701cfa07a6e14d432be48d2b7fefff1a5b0
Anton Mitsengendler <a.mic@rtc.ru>
2018-07-12 12:28:21 +0300

    Добавил методы загрузки изображения в массив

---
+++ SimpleDrawer/SpriteHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace SimpleDrawer
+{
+    static class SpriteHelper
+    {
+        static byte[] LoadImage(Uri uri, int bytesPerPixel)
+        {
+            var image = new BitmapImage(uri);
+            // Array containing pixels data
+            var buffer = new byte[image.PixelHeight * image.PixelHeight * bytesPerPixel];
+            // Bytes per 1 row of image
+            var stride = image.PixelWidth * bytesPerPixel;
+            // Write pixels to array
+            image.CopyPixels(image.SourceRect, buffer, stride, 0);
+            return buffer;
+        }
+
+        public static byte[] LoadImage(string path, int bytesPerPixel = 4)
+        {
+            return LoadImage(new Uri(path, UriKind.Relative), bytesPerPixel);
+        }
+    }
+}
